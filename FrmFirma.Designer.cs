d4f7d2e238d95a44a454d276b65e66f42ec4ddc3
joaquin Ledo <joaquin-ledo@hotmail.com>
2024-04-25 10:51:16 -0300

    Agregar archivos de proyecto.

---
+++ FrmFirma.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PryLedoEtapa1
+{
+    partial class FrmFirma
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmFirma));
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.pbFirma = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFirma)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.Color.LightCyan;
+            this.btnBorrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBorrar.Location = new System.Drawing.Point(63, 418);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(153, 35);
+            this.btnBorrar.TabIndex = 8;
+            this.btnBorrar.Text = "BORRAR";
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.LightCyan;
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnGuardar.Location = new System.Drawing.Point(279, 418);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(153, 35);
+            this.btnGuardar.TabIndex = 7;
+            this.btnGuardar.Text = "GUARDAR";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // pbFirma
+            // 
+            this.pbFirma.BackColor = System.Drawing.Color.White;
+            this.pbFirma.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbFirma.Location = new System.Drawing.Point(63, 24);
+            this.pbFirma.Name = "pbFirma";
+            this.pbFirma.Size = new System.Drawing.Size(369, 376);
+            this.pbFirma.TabIndex = 6;
+            this.pbFirma.TabStop = false;
+            // 
+            // FrmFirma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(492, 474);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.pbFirma);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmFirma";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Firma";
+            this.Load += new System.EventHandler(this.FrmFirma_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbFirma)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.PictureBox pbFirma;
+    }
+}
+
---
+++ FrmFirma.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryLedoEtapa1
+{
+    public partial class FrmFirma : Form
+    {
+        public FrmFirma()
+        {
+            InitializeComponent();
+        }
+
+        // Variables para el dibujo
+        private bool dibujando = false;
+        private Point posicionAnterior;
+        private Bitmap firmaGuardada;
+        private Bitmap firmaBitmap;
+
+        private void FrmFirma_Load(object sender, EventArgs e)
+        {
+            // Crea un Bitmap para almacenar la firma
+            firmaBitmap = new Bitmap(pbFirma.Width, pbFirma.Height);
+            pbFirma.Image = firmaBitmap;
+
+            // Inicializa los eventos del mouse
+            pbFirma.MouseDown += pbFirma_MouseDown;
+            pbFirma.MouseMove += pbFirma_MouseMove;
+            pbFirma.MouseUp += pbFirma_MouseUp;
+        }
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string carpetaImagenesFirmas = Path.Combine(Application.StartupPath, "Firmas");
+
+            if (!Directory.Exists(carpetaImagenesFirmas))
+            {
+                Directory.CreateDirectory(carpetaImagenesFirmas);
+            }
+
+            string nombreArchivo = $"firma_{DateTime.Now.ToString("yyyy-MM-dd-HH,mm,ss")}.png";
+
+            string rutaArchivo = Path.Combine(carpetaImagenesFirmas, nombreArchivo);
+
+            firmaBitmap.Save(rutaArchivo, ImageFormat.Png);
+
+            MessageBox.Show("¡Firma guardada con éxito!");
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            firmaBitmap = new Bitmap(pbFirma.Width, pbFirma.Height);
+            pbFirma.Image = firmaBitmap;
+        }
+
+        private void pbFirma_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Comenzar el dibujo cuando se presione el botón izquierdo del mouse
+            if (e.Button == MouseButtons.Left)
+            {
+                dibujando = true;
+                posicionAnterior = e.Location;
+            }
+        }
+
+        private void pbFirma_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Dibujar cuando se mueva el mouse mientras se mantiene presionado el botón izquierdo
+            if (dibujando)
+            {
+                using (Graphics g = Graphics.FromImage(firmaBitmap))
+                {
+                    // Dibujar una línea desde la posición anterior a la posición actual
+                    Pen pen = new Pen(Color.Black, 2); // Puedes ajustar el color y el grosor aquí
+                    g.DrawLine(pen, posicionAnterior, e.Location);
+                }
+                // Actualizar la posición anterior con la nueva posición
+                posicionAnterior = e.Location;
+                pbFirma.Invalidate(); // Vuelve a dibujar el PictureBox
+            }
+        }
+
+        private void pbFirma_MouseUp(object sender, MouseEventArgs e)
+        {
+            // Terminar el dibujo cuando se suelte el botón izquierdo del mouse
+            if (e.Button == MouseButtons.Left)
+            {
+                dibujando = false;
+            }
+        }
+
+
+    }
+}
